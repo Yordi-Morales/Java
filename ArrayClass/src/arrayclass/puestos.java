60e6ab146efacef97d51c8707f6e9b65d58bd6eb
Yordi Morales Vargas <106575394+Yordi-Morales@users.noreply.github.com>
2024-01-10 20:15:18 -0600

    Add files via upload
    Array class workshop

---
+++ ArrayClass/src/arrayclass/puestos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayclass;
+
+/**
+ *
+ * @author Y.Morales
+ */
+public class puestos {
+
+    private String nombre;
+    private String Siglas;
+    private String Salario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSiglas() {
+        return Siglas;
+    }
+
+    public void setSiglas(String Siglas) {
+        this.Siglas = Siglas;
+    }
+
+    public String getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(String Salario) {
+        this.Salario = Salario;
+    }
+}
